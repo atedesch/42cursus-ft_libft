ab1f2803bd1d4ef6be11b6b609cfb3b7d6d69c04
atedesch <andrea.a.tedeschi@gmail.com>
2024-01-20 19:14:48 +0100

    24-01-20_v06-01

--- ft_memcmp.c
+++ ft_memcmp.c
@@ -12,10 +12,10 @@
 
 #include "libft.h"
 
-int	ft_memcmp(const void *str1, const void *str2, size_t n)
+int ft_memcmp(const void *str1, const void *str2, size_t n)
 {
-	const unsigned char	*s1;
-	const unsigned char	*s2;
+	const unsigned char *s1;
+	const unsigned char *s2;
 
 	s1 = (const unsigned char *)str1;
 	s2 = (const unsigned char *)str2;
@@ -25,7 +25,7 @@ int	ft_memcmp(const void *str1, const void *str2, size_t n)
 		s2++;
 		n--;
 	}
-	if (n && (s1 != '\0' || s2 != '\0'))
+	if (n && (s1 != NULL || s2 != NULL))
 		return (*s1 - *s2);
 	return (0);
 }
