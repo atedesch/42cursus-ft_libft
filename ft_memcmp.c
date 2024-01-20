12d07858a5c3018da9fb6b16c4a58b4ea80aaa4d
atedesch <andrea.a.tedeschi@gmail.com>
2024-01-20 19:29:31 +0100

    24-01-20_v06-02

--- ft_memcmp.c
+++ ft_memcmp.c
@@ -3,19 +3,19 @@
 /*                                                        :::      ::::::::   */
 /*   ft_memcmp.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: atedesch <marvin@42.fr>                    +#+  +:+       +#+        */
+/*   By: atedesch <atedesch@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/01/17 15:13:23 by atedesch          #+#    #+#             */
-/*   Updated: 2024/01/17 15:34:39 by atedesch         ###   ########.fr       */
+/*   Created: 2024/01/20 19:18:52 by atedesch          #+#    #+#             */
+/*   Updated: 2024/01/20 19:18:52 by atedesch         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-int ft_memcmp(const void *str1, const void *str2, size_t n)
+int	ft_memcmp(const void *str1, const void *str2, size_t n)
 {
-	const unsigned char *s1;
-	const unsigned char *s2;
+	const unsigned char	*s1;
+	const unsigned char	*s2;
 
 	s1 = (const unsigned char *)str1;
 	s2 = (const unsigned char *)str2;
